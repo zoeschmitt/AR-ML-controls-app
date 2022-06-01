813e473e9f70d41c4a3575f30022a3a5c0ff89f5
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-05-31 19:54:38 -0500

    feat: added utils

---
+++ AR-MachineLearning/Utils/Node+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Node+Extensions.swift
+//  Created by Zoe Schmitt on 5/29/22.
+//
+
+import SceneKit
+
+extension SCNNode {
+
+    public class func allNodes(from file: String) -> [SCNNode] {
+        var nodesInFile = [SCNNode]()
+
+        do {
+            guard let sceneURL = Bundle.main.url(forResource: file, withExtension: nil) else {
+                print("Could not find scene file \(file)")
+                return nodesInFile
+            }
+
+            let objScene = try SCNScene(url: sceneURL as URL, options: [SCNSceneSource.LoadingOption.animationImportPolicy: SCNSceneSource.AnimationImportPolicy.doNotPlay])
+
+            objScene.rootNode.enumerateChildNodes({ (node, _) in
+                nodesInFile.append(node)
+            })
+        } catch {
+
+        }
+
+        return nodesInFile
+    }
+}
